ca878bbbfa7eb7cabf6e89ee80a94c76ab3d0ff8
bin2812 <lquanghuy218@gmail.com>
2025-02-22 11:54:44 +0700

    cart

--- src/components/WebBook/OutletLayout.tsx
+++ src/components/WebBook/OutletLayout.tsx
@@ -17,13 +17,9 @@ const LayoutRoute: React.FC = () => {
             <div className="item-img">
               <img src="https://lh3.googleusercontent.com/bhg6FoxZPA5S6q50_Gmu6YgMt2GcKxpjsbnaRvvHUwSV9JSnAj83TpbJEHIJjgKnbNoZmM7Mnx0pBlUau5CG2yIOG5v7rGc=w100-rw" />
             </div>
-            <div className="item-info">
+            <div className="item-info flex flex-1 gap-4 justify-around">
               <div className="item-name">
                 <h3>Book Name</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam, quos.
-                </p>
               </div>
               <div className="item-price">
                 <span>100$</span>
@@ -35,8 +31,8 @@ const LayoutRoute: React.FC = () => {
           </div>
         </div>
         <div className="btn">
-          <button class="close">CLOSE</button>
-          <button class="checkOut">Check Out</button>
+          <button className="close">CLOSE</button>
+          <button className="checkOut">Check Out</button>
         </div>
       </div>
     </div>
